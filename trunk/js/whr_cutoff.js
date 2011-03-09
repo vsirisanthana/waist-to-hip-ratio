928960a8735d608d965edc578dd3ad7379a81a71
vsirisanthana@gmail.com <vsirisanthana@gmail.com@837367e2-af9f-4a2e-7dac-9e45d8c8d0e2>
2011-03-09 15:07:29 +0000

    - Upload initial source codes

---
+++ trunk/js/whr_cutoff.js
@@ -0,0 +1,233 @@
+var whr_cutoff = [
+    {
+        "male-overweight": "18.41", 
+        "age": "2.0", 
+        "female-obesity": "19.81", 
+        "female-overweight": "18.02", 
+        "male-obesity": "20.09"
+    }, 
+    {
+        "male-overweight": "18.13", 
+        "age": "2.5", 
+        "female-obesity": "19.55", 
+        "female-overweight": "17.76", 
+        "male-obesity": "19.80"
+    }, 
+    {
+        "male-overweight": "17.89", 
+        "age": "3.0", 
+        "female-obesity": "19.36", 
+        "female-overweight": "17.56", 
+        "male-obesity": "19.57"
+    }, 
+    {
+        "male-overweight": "17.69", 
+        "age": "3.5", 
+        "female-obesity": "19.23", 
+        "female-overweight": "17.40", 
+        "male-obesity": "19.39"
+    }, 
+    {
+        "male-overweight": "17.55", 
+        "age": "4.0", 
+        "female-obesity": "19.15", 
+        "female-overweight": "17.28", 
+        "male-obesity": "19.29"
+    }, 
+    {
+        "male-overweight": "17.47", 
+        "age": "4.5", 
+        "female-obesity": "19.12", 
+        "female-overweight": "17.19", 
+        "male-obesity": "19.26"
+    }, 
+    {
+        "male-overweight": "17.42", 
+        "age": "5.0", 
+        "female-obesity": "19.17", 
+        "female-overweight": "17.15", 
+        "male-obesity": "19.30"
+    }, 
+    {
+        "male-overweight": "17.45", 
+        "age": "5.5", 
+        "female-obesity": "19.34", 
+        "female-overweight": "17.20", 
+        "male-obesity": "19.47"
+    }, 
+    {
+        "male-overweight": "17.55", 
+        "age": "6.0", 
+        "female-obesity": "19.65", 
+        "female-overweight": "17.34", 
+        "male-obesity": "19.78"
+    }, 
+    {
+        "male-overweight": "17.71", 
+        "age": "6.5", 
+        "female-obesity": "20.08", 
+        "female-overweight": "17.53", 
+        "male-obesity": "20.23"
+    }, 
+    {
+        "male-overweight": "17.92", 
+        "age": "7.0", 
+        "female-obesity": "20.51", 
+        "female-overweight": "17.75", 
+        "male-obesity": "20.63"
+    }, 
+    {
+        "male-overweight": "18.16", 
+        "age": "7.5", 
+        "female-obesity": "21.01", 
+        "female-overweight": "18.03", 
+        "male-obesity": "21.09"
+    }, 
+    {
+        "male-overweight": "18.44", 
+        "age": "8.0", 
+        "female-obesity": "21.57", 
+        "female-overweight": "18.35", 
+        "male-obesity": "21.60"
+    }, 
+    {
+        "male-overweight": "18.76", 
+        "age": "8.5", 
+        "female-obesity": "22.18", 
+        "female-overweight": "18.69", 
+        "male-obesity": "22.17"
+    }, 
+    {
+        "male-overweight": "19.10", 
+        "age": "9.0", 
+        "female-obesity": "22.81", 
+        "female-overweight": "19.07", 
+        "male-obesity": "22.77"
+    }, 
+    {
+        "male-overweight": "19.46", 
+        "age": "9.5", 
+        "female-obesity": "23.46", 
+        "female-overweight": "19.45", 
+        "male-obesity": "23.39"
+    }, 
+    {
+        "male-overweight": "19.84", 
+        "age": "10.0", 
+        "female-obesity": "24.11", 
+        "female-overweight": "19.86", 
+        "male-obesity": "24.00"
+    }, 
+    {
+        "male-overweight": "20.20", 
+        "age": "10.5", 
+        "female-obesity": "24.77", 
+        "female-overweight": "20.29", 
+        "male-obesity": "24.57"
+    }, 
+    {
+        "male-overweight": "20.55", 
+        "age": "11.0", 
+        "female-obesity": "25.42", 
+        "female-overweight": "20.74", 
+        "male-obesity": "25.10"
+    }, 
+    {
+        "male-overweight": "20.89", 
+        "age": "11.5", 
+        "female-obesity": "26.05", 
+        "female-overweight": "21.20", 
+        "male-obesity": "25.58"
+    }, 
+    {
+        "male-overweight": "21.22", 
+        "age": "12.0", 
+        "female-obesity": "26.67", 
+        "female-overweight": "21.68", 
+        "male-obesity": "26.02"
+    }, 
+    {
+        "male-overweight": "21.56", 
+        "age": "12.5", 
+        "female-obesity": "27.24", 
+        "female-overweight": "22.14", 
+        "male-obesity": "26.43"
+    }, 
+    {
+        "male-overweight": "21.91", 
+        "age": "13.0", 
+        "female-obesity": "27.76", 
+        "female-overweight": "22.58", 
+        "male-obesity": "26.84"
+    }, 
+    {
+        "male-overweight": "22.27", 
+        "age": "13.5", 
+        "female-obesity": "28.20", 
+        "female-overweight": "22.98", 
+        "male-obesity": "27.25"
+    }, 
+    {
+        "male-overweight": "22.62", 
+        "age": "14.0", 
+        "female-obesity": "28.57", 
+        "female-overweight": "23.34", 
+        "male-obesity": "27.63"
+    }, 
+    {
+        "male-overweight": "22.96", 
+        "age": "14.5", 
+        "female-obesity": "28.87", 
+        "female-overweight": "23.66", 
+        "male-obesity": "27.98"
+    }, 
+    {
+        "male-overweight": "23.29", 
+        "age": "15.0", 
+        "female-obesity": "29.11", 
+        "female-overweight": "23.94", 
+        "male-obesity": "28.30"
+    }, 
+    {
+        "male-overweight": "23.60", 
+        "age": "15.5", 
+        "female-obesity": "29.29", 
+        "female-overweight": "24.17", 
+        "male-obesity": "28.60"
+    }, 
+    {
+        "male-overweight": "23.90", 
+        "age": "16.0", 
+        "female-obesity": "29.43", 
+        "female-overweight": "24.37", 
+        "male-obesity": "28.88"
+    }, 
+    {
+        "male-overweight": "24.19", 
+        "age": "16.5", 
+        "female-obesity": "29.56", 
+        "female-overweight": "24.54", 
+        "male-obesity": "29.14"
+    }, 
+    {
+        "male-overweight": "24.46", 
+        "age": "17.0", 
+        "female-obesity": "29.69", 
+        "female-overweight": "24.70", 
+        "male-obesity": "29.41"
+    }, 
+    {
+        "male-overweight": "24.73", 
+        "age": "17.5", 
+        "female-obesity": "29.84", 
+        "female-overweight": "24.85", 
+        "male-obesity": "29.70"
+    }, 
+    {
+        "male-overweight": "25.00", 
+        "age": "18.0", 
+        "female-obesity": "30.00", 
+        "female-overweight": "25.00", 
+        "male-obesity": "30.00"
+    }
+]
